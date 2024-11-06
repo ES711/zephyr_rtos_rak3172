6c5c96895d8b52e6e8884e95e2e401c358d3ef55
YoMin Su <m11317004@yuntech.edu.tw>
2024-11-06 10:20:24 +0800

    2024/11/06
    調整板子的定義，在DTS中預設啟用硬體設計的周邊。
    將LoRa相關的選項調整為預設關閉，降低映像編譯完的大小。
    加入JLink相關的CMake工具引用，讓Debug模式可使用。

--- board.cmake
+++ board.cmake
@@ -1,8 +1,8 @@
-# SPDX-License-Identifier: Apache-2.0
-
-# keep first
+# Enable second one when using JLink
 board_runner_args(stm32cubeprogrammer "--port=swd" "--reset-mode=hw")
+#board_runner_args(stm32cubeprogrammer "--port=jlink reset=HWrst freq=4000")
+board_runner_args(jlink "--device=STM32WLE5CC" "--speed=4000" "--reset-after-load")
 
 # keep first
 include(${ZEPHYR_BASE}/boards/common/stm32cubeprogrammer.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
